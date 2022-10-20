91a0841307d95538366dca0e4c132e743dd1f315
asus <casadadaista@gmail.com>
2022-10-20 18:26:41 -0300

    for fakesies

--- src/views/admin/LunchMenuAdmin.svelte
+++ src/views/admin/LunchMenuAdmin.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <div>
+  <h1>this is for fakesies</h1>
   <h1 class="title is-4">Lunch Menu Administration / {schoolName}</h1>
 </div>
 <!-- <h1 class="title is-4">Lunch Menu Administration / {$user.schoolName}</h1> -->
