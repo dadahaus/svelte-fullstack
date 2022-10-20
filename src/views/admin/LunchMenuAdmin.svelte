fc7552d6579ee4f85b92fa4f8e65a0224e43cbef
asus <casadadaista@gmail.com>
2022-10-20 18:44:51 -0300

    last working copy

--- src/views/admin/LunchMenuAdmin.svelte
+++ src/views/admin/LunchMenuAdmin.svelte
@@ -1,8 +1,8 @@
 <script>
-  let schoolName = 'Test School'
+  import {user} from '../../stores'
 </script>
 
 <div>
-  <h1 class="title is-4">Lunch Menu Administration / {schoolName}</h1>
+  <!-- <h1 class="title is-4">Lunch Menu Administration / {schoolName}</h1> -->
+  <h1 class="title is-4">Lunch Menu Administration / {$user.schoolName}</h1>
 </div>
-<!-- <h1 class="title is-4">Lunch Menu Administration / {$user.schoolName}</h1> -->
