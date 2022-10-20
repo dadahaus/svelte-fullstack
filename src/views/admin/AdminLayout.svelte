3caeba5808100a514832445de124f7b90b238c26
asus <casadadaista@gmail.com>
2022-10-20 18:40:57 -0300

    succesful repair

--- src/views/admin/AdminLayout.svelte
+++ src/views/admin/AdminLayout.svelte
@@ -1,9 +1,21 @@
 <script>
   import { Route } from 'svelte-router-spa'
+  import {onMount} from 'svelte'
+  import {user} from '../../stores'
   export let currentRoute
+  let initialized = false
+  onMount(()=>{
+    user.set({
+      name: 'test user',
+      schoolName: 'Test School',
+
+    })
+    initialized = true
+  })
 </script>
 
 <div>
-  Admin Layout
+  {#if initialized}
   <Route {currentRoute} />
+  {/if}
 </div>
